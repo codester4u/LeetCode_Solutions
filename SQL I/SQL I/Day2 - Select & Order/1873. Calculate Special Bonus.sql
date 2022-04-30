5b08c23a39f097a40284afe8cc9be1357a0b603d
~Arjun~ <83962241+codester4u@users.noreply.github.com>
2022-04-30 11:15:58 +0530

    Delete SQL I/SQL I directory

--- SQL I/SQL I/Day2 - Select & Order/1873. Calculate Special Bonus.sql
+++
@@ -1,1 +0,0 @@
-SELECT employee_id, IF(employee_id % 2 != 0 and SUBSTRING(name, 1, 1) != 'M', salary, 0) AS bonus from Employees;
--- SQL I/SQL I/Day2 - Select & Order/196. Delete Duplicate Emails.sql
+++
@@ -1,5 +0,0 @@
-WITH cte AS (
-SELECT Id, EMail, ROW_NUMBER() OVER(PARTITION BY Email ORDER BY Id) as row_num FROM Person)
-
-DELETE FROM Person
-WHERE Id IN (SELECT id FROM cte WHERE row_num > 1 )
