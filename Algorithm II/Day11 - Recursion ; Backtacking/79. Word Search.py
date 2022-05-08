fab922efad7d5cccbec4e87157036abb0fb615c5
~Arjun~ <83962241+codester4u@users.noreply.github.com>
2022-05-08 12:54:23 +0530

    Day 11

---
+++ Algorithm II/Day11 - Recursion ; Backtacking/79. Word Search.py
@@ -0,0 +1,20 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        N=len(board[0])
+        M=len(board)
+        p=len(word)
+        
+        def helper(r, c, pos):
+            if pos>=p: return True
+            elif 0<=r<M and 0<=c<N and board[r][c]==word[pos]:
+                temp=board[r][c]
+                board[r][c]=None
+                
+                if helper(r-1, c, pos+1) or helper(r+1, c, pos+1) or helper(r, c-1, pos+1) or helper(r, c+1, pos+1):
+                    return True
+                board[r][c]=temp
+            return False
+        for r in range(M):
+            for c in range(N):
+                if helper(r, c, 0): return True
+        return False
