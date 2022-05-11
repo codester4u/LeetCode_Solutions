e4edaa7440e460d769a763178df47ddf37f1732d
~Arjun~ <83962241+codester4u@users.noreply.github.com>
2022-05-11 10:48:56 +0530

    Delete Day 9 directory

--- Day 9/1337. The K Weakest Rows in a Matrix.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        dic={}
-        for i in range(len(mat)):
-            dic[i]=sum(mat[i])
-        l = sorted(dic.items(), key=lambda x:x[1])
-        l=dict(l)
-        x=list(l.keys())
-        return x[:k]
